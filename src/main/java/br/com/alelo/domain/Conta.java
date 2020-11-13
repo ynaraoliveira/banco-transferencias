c66908abcee344b794c41dfe4b1c7bd595a095fc
ynaraoliveira <ynaraoliveira.s@gmail.com>
2020-11-12 23:47:25 -0300

    add table tag

--- src/main/java/br/com/alelo/domain/Conta.java
+++ src/main/java/br/com/alelo/domain/Conta.java
@@ -1,5 +1,9 @@
 package br.com.alelo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -9,17 +13,24 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @Table(name = "conta")
 public class Conta {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
 	
+	@Column(name = "nome")
 	private String nome;
 	
+	@Column(name = "documento")
 	private String documento;
 	
+	@Column(name = "conta")
 	private String conta;
 	
     @OneToOne(mappedBy = "conta")
     private Transferencia transferencia;
 	
+    @Column(name = "saldoDebito")
 	private double saldoDebito;
 	
 	public Conta(String nome, String documento, String conta, double saldoDebito, double saldoCredito) {
